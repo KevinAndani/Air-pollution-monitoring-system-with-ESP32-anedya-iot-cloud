71e6d1ad2913def51933e21181642c0046f7f6a4
KevinAndani <andani.kevin1610@gmail.com>
2024-07-16 11:10:52 +0530

    Use the static global variables

--- IoT_dashboard/New_Dashboard.py
+++ IoT_dashboard/New_Dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from utils.pageconfig import page_config
 from utils.anedya import anedya_config, fetchHumidityData, fetchTemperatureData, GetFanStatus, GetHumidifierStatus
-# from utils.global_vars import nodeId, apiKey
+from utils.global_vars import nodeId, apiKey
 from utils.action_buttons import ButtonText, State, CurrentData
 from utils.login import drawLogin
 from utils.dashboard import drawDashboard
@@ -11,8 +11,8 @@ from utils.dashboard import drawDashboard
 page_config()
 
 def main():
-    # global nodeId, apiKey
-    # anedya_config(NODE_ID=nodeId,API_KEY=apiKey)
+    global nodeId, apiKey
+    anedya_config(NODE_ID=nodeId,API_KEY=apiKey)
 
     if"LoggedIn" not in st.session_state:
         st.session_state.LoggedIn = False
